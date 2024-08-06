4db7eaadb92b2f0d94c6c29d85df2e6a0c208a03
Keiran M Raine <keiranmraine@users.noreply.github.com>
2024-08-06 11:41:37 +0100

    fix path

--- build.sh
+++ build.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 set -eu
 
+## cleanup
+rm -rf build/
+
 echo '### Creating base files for package ###'
-mkdir -p $PACKAGE/src/$PACKAGE
-mkdir -p $PACKAGE/tests
-cp src/python-dummy/__init__.py $PACKAGE/src/$PACKAGE/__init_.py
-cp src/python-dummy/__init__.py $PACKAGE/src/$PACKAGE/blocked.py
+mkdir -p build/$PACKAGE/src/$PACKAGE
+mkdir -p build/$PACKAGE/tests
+cp src/python-dummy/__init__.py build/$PACKAGE/src/$PACKAGE/__init_.py
+cp src/python-dummy/__init__.py build/$PACKAGE/src/$PACKAGE/blocked.py
 # we don't change this file
-cp README.md $PACKAGE/.
-cp LICENSE $PACKAGE/.
+cp README.md build/$PACKAGE/.
+cp LICENSE build/$PACKAGE/.
 ## changes to pyproject.toml here:
-cat pyproject.toml | perl -ne "\$_ =~ s/python-dummy/$PACKAGE/g; print \$_;" > $PACKAGE/pyproject.toml
+cat pyproject.toml | perl -ne "\$_ =~ s/python-dummy/$PACKAGE/g; print \$_;" > build/$PACKAGE/pyproject.toml
 
 echo -e '\n\n### Creating package build environment ###'
-cd $PACKAGE
+cd build/$PACKAGE
 python -m venv .venv
 source .venv/*/activate
 python -m pip install --upgrade build twine
